d4dc5012cf0de570558e41beec8fde8ea8f1f231
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 17:57:24 +0000

    Adding tarfile member sanitization to extractall()

--- gobbli/util.py
+++ gobbli/util.py
@@ -617,7 +617,26 @@ def blob_to_dir(blob: bytes, dir_path: Path):
       dir_path: Path to extract the directory to.
     """
     with tarfile.open(fileobj=io.BytesIO(blob), mode="r:gz") as archive:
-        archive.extractall(dir_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, dir_path)
 
 
 T1 = TypeVar("T1")
